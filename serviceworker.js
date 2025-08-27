3ea5249ec354bde419e87e18b3436b302031a3b1
fynn23 <54664196+fynn23@users.noreply.github.com>
2025-08-27 19:25:18 +0200

    00

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const version = 3254;
+const version = 3255;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
