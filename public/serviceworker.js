b2cb5a44bd5301ca0e76a19b773cbf6bc6478023
Fynn Schmidt (via WeWeb) <admin@tillyboard.com>
2025-09-02 11:09:48 +0200

    v3284 - 00

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 3283;
+const version = 3284;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
